67ca10ccf2a3561446f9b6746e2241b163ebf8ec
Oksana Prokopenko <oksana.prokopenko@p2h.com>
2023-02-14 09:40:52 +0400

    add typeScript consts

--- app.ts
+++ app.ts
@@ -2,7 +2,40 @@ const isActive: boolean = true;
 const heading: string = 'My Heading';
 const year: number = 20;
 const sizes: number[] = [3,7,10,15];
+
 function printSizes(obj: { size: string }) {
     return obj;
 }
 printSizes({size: "10"})
+
+function printError(): void {
+    console.log('Error')
+}
+
+interface Pet {
+    name: string;
+    age: number;
+    legs: number | boolean;
+    hasWool: boolean;
+    colors: []
+}
+
+function callPet(animal: Pet) {
+    return `Hi it's ${animal.name}, it's ${animal.legs}legs`;
+}
+
+function sum(x: number, y: number): number {
+    return x + y;
+}
+
+function printInfo(name: string, numb: number) {
+    console.log(`${name} has ${numb} bags`)
+}
+
+function identity <T>(value: T) : T {
+    return value;
+}
+
+console.log(identity<Number>(1))
+console.log(identity<String>("1"))
+console.log(identity<number[]>([1, 2]))
--- js/index.js
+++ js/index.js
@@ -7,4 +7,22 @@ function printSizes(obj) {
     return obj;
 }
 printSizes({ size: "10" });
+function printError() {
+    console.log('Error');
+}
+function callPet(animal) {
+    return `Hi it's ${animal.name}, it's ${animal.legs}legs`;
+}
+function sum(x, y) {
+    return x + y;
+}
+function printInfo(name, numb) {
+    console.log(`${name} has ${numb} bags`);
+}
+function identity(value) {
+    return value;
+}
+console.log(identity(1));
+console.log(identity("1"));
+console.log(identity([1, 2]));
 console.log('modulo js 445');
